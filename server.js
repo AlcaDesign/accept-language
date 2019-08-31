3eb66fe3267070ff7ffc17f5807db119e19eee1e
Jacob Foster <jfost3@gmail.com>
2019-08-31 00:00:25 -0500

    Add CORS and respond with JSON

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const { PORT } = require('dotenv').config().parsed;
 const server = require('http').createServer((req, res) => {
+	res.setHeader('access-control-allow-origin', '*');
+	res.setHeader('content-type', 'application/json; charset=utf-8');
 	try {
-		res.write(req.headers['accept-language'] || '');
-		res.end();
-	} catch {}
+		const data = {
+			language: req.headers['accept-language'] || '',
+			status: 200
+		};
+		const body = JSON.stringify(data);
+		res.statusCode = 200;
+		res.write(body);
+	} catch {
+		res.statusCode = 500;
+		res.write('{"language":null,"status":500}');
+	}
+	res.end();
 }).listen(PORT, () => console.log('Listening :' + server.address().port));
